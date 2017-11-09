b43075f33db85214ec2b2a64daf4d2c6df34fa4b
Harold Portocarrero <hportocarrero@todo1.com>
2017-11-09 09:28:15 -0500

    ejemplo de Genericos

---
+++ src/com/arquitecturajava/ejemplo001/Bombon.java
@@ -0,0 +1,20 @@
+package com.arquitecturajava.ejemplo001;
+
+public class Bombon {
+	
+	private String tipo;
+
+	public Bombon(String tipo) {
+		super();
+		this.tipo = tipo;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+}
---
+++ src/com/arquitecturajava/ejemplo001/Galleta.java
@@ -0,0 +1,20 @@
+package com.arquitecturajava.ejemplo001;
+
+public class Galleta {
+	
+	private String sabor;
+
+	public Galleta(String sabor) {
+		super();
+		this.sabor = sabor;
+	}
+
+	public String getSabor() {
+		return sabor;
+	}
+
+	public void setSabor(String sabor) {
+		this.sabor = sabor;
+	}
+
+}
